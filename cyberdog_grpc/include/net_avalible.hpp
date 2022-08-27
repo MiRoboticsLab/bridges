d06dcc55aef43ff8951870a1b67cb5e51d99acfe
wenlinfeng <wenlinfeng@xiaomi.com>
2022-08-27 12:10:49 +0800

    [Fix] Optimize thread of NetChecker
    1.优化了NetChecker中子线程的使用，用条件变量代替sleep
    
    Signed-off-by: wenlinfeng <wenlinfeng@xiaomi.com>

--- cyberdog_grpc/include/net_avalible.hpp
+++ cyberdog_grpc/include/net_avalible.hpp
@@ -21,38 +21,53 @@
 #include <memory>
 #include <string>
 #include <thread>
+#include <mutex>
+#include <condition_variable>
+#include <chrono>
 
 #include "cyberdog_common/cyberdog_log.hpp"
 class NetChecker
 {
 public:
   NetChecker()
-  : need_run_(true), thread_(NULL), need_ping(true) {}
+  : need_run_(true), thread_(nullptr), need_ping_(false)
+  {
+    run();
+  }
   ~NetChecker()
   {
-    need_run_ = false;
-    if (thread_ != NULL) {
+    {
+      std::unique_lock<std::mutex> lck(ip_mutex_);
+      need_run_ = false;
+      cv_.notify_all();
+    }
+    if (thread_ && thread_->joinable()) {
       thread_->join();
     }
   }
-  void pause()
+  void pause(bool pau = true)
   {
-    need_ping = false;
-    ip_ = "";
+    std::unique_lock<std::mutex> lck(ip_mutex_);
+    need_ping_ = !pau;
+    cv_.notify_one();
   }
-  void set_ip(std::string ip)
+  void set_ip(const std::string & ip, bool start = true)
   {
-    if (ip != ip_) {
-      ip_ = ip;
-      need_ping = true;
-      run();
+    {
+      std::unique_lock<std::mutex> lck(ip_mutex_);
+      if (ip != ip_) {
+        ip_ = ip;
+      }
+    }
+    if (start && !ip.empty()) {
+      pause(false);
     }
   }
 
 private:
   void run()
   {
-    if (thread_ == NULL) {
+    if (!thread_) {
       thread_ = std::make_shared<std::thread>(&NetChecker::run_, this);
     }
   }
@@ -61,26 +76,36 @@ private:
   {
     FILE * fp;
     char buf[128];
+    std::unique_lock<std::mutex> lck(ip_mutex_);
     while (need_run_) {
-      if (need_ping) {
-        std::string cmd = "ping -c 1 -w 2 " + ip_;
-        if ((fp = popen(cmd.c_str(), "r")) == NULL) {
-          ERROR("failed to popen");
-          return;
-        }
-
-        while (fgets(buf, sizeof(buf), fp) != NULL) {
-          INFO_STREAM(buf);
-        }
-        pclose(fp);
+      if (need_ping_) {
+        cv_.wait_for(lck, std::chrono::seconds(10));
+      } else {
+        cv_.wait(lck);
       }
-      sleep(10);
+      if (!need_run_) {
+        break;
+      }
+      if (!need_ping_) {
+        continue;
+      }
+      std::string cmd = "ping -c 1 -w 2 " + ip_;
+      if ((fp = popen(cmd.c_str(), "r")) == NULL) {
+        ERROR("failed to popen");
+        continue;
+      }
+      while (fgets(buf, sizeof(buf), fp) != NULL) {
+        INFO_STREAM(buf);
+      }
+      pclose(fp);
     }
   }
   std::shared_ptr<std::thread> thread_;
   std::string ip_;
   bool need_run_;
-  bool need_ping;
+  bool need_ping_;
+  std::mutex ip_mutex_;
+  std::condition_variable cv_;
 };
 
 #endif  // NET_AVALIBLE_HPP_
